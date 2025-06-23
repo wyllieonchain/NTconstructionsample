f555196b3cc83e1c619ac31aecca2022c93cc518
malcolm21 <malcolm@buzzmarkets.io>
2025-06-23 13:33:11 -0400

    fix book text sizing

--- src/app/book-a-consultation/page.tsx
+++ src/app/book-a-consultation/page.tsx
@@ -15,7 +15,7 @@ export default function BookAConsultation() {
         />
         <div className="absolute inset-0 bg-black/70 -z-10" />
         <div className="text-center">
-          <h1 className="text-7xl font-bold mb-8">Book a Consultation</h1>
+          <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold mb-8">Book a Consultation</h1>
           <div className="flex justify-center space-x-4">
             <a href="mailto:Nick@ntconstructionltd.ca" className="bg-white hover:bg-gray-300 text-black font-bold py-4 px-8 rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300 ease-in-out">
               Email Us
